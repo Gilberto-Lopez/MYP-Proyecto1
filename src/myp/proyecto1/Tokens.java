c836234a2efd10a3d090635cb58cab172fc8f41e
Gilberto-Lopez <gilisaac@ciencias.unam.mx>
2015-09-03 19:32:58 -0500

    Finalizado Tokens.java, agregadas pruebas unitarias correspondientes TestTokens.java

--- src/myp/proyecto1/Tokens.java
+++ src/myp/proyecto1/Tokens.java
@@ -4,27 +4,55 @@ import mx.unam.ciencias.edd.Lista;
 
 /**
  * Clase para obtener tokens (fichas) a partir de un String donde cada token es
- * una 
+ * una expresión dada por la gramática especificada por el proyecto.
  */
 public class Tokens{
 
     /**
-     *
+     * Método que recibe como entrada un String el cual será dividido en tokens
+     * para posteriormente colocarlos en una lista, siempre respetando el orden
+     * en que aparecen en la cadena de entrada.
+     * @param cadena Un String a partir del cual obtendremos los tokens.
+     * @return Una lista con los tokens del String <code>cadena</code>.
+     * @throws ExcepcionExpresionInvalida en caso de que <code>cadena</code> sea
+     *         una cadena vacía o una referencia null.
      */
     public Lista<String> obtenerTokens(String cadena){
-	char[] caracteres = cadena.replaceAll(" ","").toCharArray();
+	if(cadena == null || cadena.equals(""))
+	    throw new ExcepcionExpresionInvalida("Asegúrese de proporcionar una expresión.");
+	char[] caracteres = cadena.toCharArray();
 	Lista<String> lista = new Lista<>();
 	for(int i = 0; i < caracteres.length; i++){
 	    char c = caracteres[i];
+	    if(c == ' ')
+		continue;
 	    if(c == 'x' || c == '+' || c == '-' || c == '*' || c == '/' ||
 	       c == '^' || c == '(' || c == ')')
-		lista.agrega(new String(c));
-	    else{
+		lista.agrega("" + c);
+	    else if((c >= '0' && c <= '9') || c == '.'){
+		String num = "" + c;
+		int j = i+1;
+		int p = (c == '.') ? 1 : 0;
+		while(j < caracteres.length &&
+		      ((caracteres[j] >= '0' && caracteres[j] <= '9') ||
+		       caracteres[j] == '.')){
+		    if(caracteres[j] == '.')
+			p++;
+		    num += caracteres[j];
+		    if(p == 2)
+			throw new ExcepcionExpresionInvalida("Expresión inválida: \"" + num + "\" -- Contiene dos puntos para decimales.");
+		    j++;
+		}
+		i = j-1;
+		lista.agrega(num);
+	    }else if(c == 's' || c == 'c' || c == 't'){
+		if(i+2 >= caracteres.length)
+		    throw new ExcepcionExpresionInvalida("\"" + c + caracteres[i+1] + "\" no es una expresión válida.");
 		char c1 = caracteres[i+1];
 		char c2 = caracteres[i+2];
 		if(c == 's' && c1 == 'i' && c2 == 'n')
 		    lista.agrega("sin");
-		else if(c == 's' && c1 == 'q' && c2 == "r")
+		else if(c == 's' && c1 == 'q' && c2 == 'r')
 		    lista.agrega("sqr");
 		else if(c == 's' && c1 == 'e' && c2 == 'c')
 		    lista.agrega("sec");
@@ -36,8 +64,11 @@ public class Tokens{
 		    lista.agrega("cot");
 		else if(c == 't' && c1 == 'a' && c2 == 'n')
 		    lista.agrega("tan");
+		else
+		    throw new ExcepcionExpresionInvalida("\"" + c + c1 + c2 + "\" no es una expresión válida.");
 		i += 2;
-	    }
+	    }else
+		throw new ExcepcionExpresionInvalida("\"" + cadena + "\" no es una expresión válida. ");
 	}
 	return lista;
     }
--- src/myp/proyecto1/test/TestTokens.java
+++ src/myp/proyecto1/test/TestTokens.java
@@ -1,19 +1,117 @@
 package myp.proyecto1.test;
 
+import java.util.Random;
 import myp.proyecto1.Tokens;
+import myp.proyecto1.ExcepcionExpresionInvalida;
 import mx.unam.ciencias.edd.Lista;
+import org.junit.Assert;
+import org.junit.Test;
 
+/**
+ * Clase para pruebas unitarias de la clase {@link Tokens}.
+ */
 public class TestTokens{
 
-    @Test public void testObtenerTokens(){
-	Tokens tokenizador = new Tokens();
-	Lista<String> tokens = tokenizador.obtenerTokens("( ^ ( + ( cos x ) ( sen x ) ) 2 )");
-	int length = lista.getLongitud();
-	assertTrue(length == 15);
-	String[] tokensManual = {"(","^","(","+","(","cos","x",")","(","sen","x",")",")","2",")"};
-	int i = 0;
-	for(String token : tokens)
-	    assertTrue(token.equals(tokensManual[i++]));
-    }	
+    private Random random;
+    private Tokens tokenizador;
+    private final String[] expresiones = {
+	"sin", "cos", "csc", "tan", "sec", "cot", "sqr", "+", "-", "*", "/",
+	"^", "(", ")", "x"
+    };
+    private final String[] numerosValidos = {
+	"12345.67890", "232324342.", ".452656462"
+    };
+    private final String[] numerosInvalidos = {
+	"123.456.789", "0.0.0.0", "255.255.255.0"
+    };
+
+    /**
+     * Crea un tokenizador para la prueba unitaria del método obtenerTokens().
+     */
+    public TestTokens(){
+	random = new Random();
+	tokenizador = new Tokens();
+    }
     
+    /**
+     * Prueba unitaria para {@link Tokens#obtenerTokens}.
+     */
+    @Test public void testObtenerTokens(){
+	int i;
+	//Prueba fija para números.
+	for(String n : numerosValidos){
+	    Lista<String> nums = tokenizador.obtenerTokens(n);
+	    Assert.assertTrue(nums.getLongitud() == 1);
+	    Assert.assertTrue(nums.getPrimero().equals(n));
+	}
+	for(String n : numerosInvalidos){
+	    try{
+		Lista<String> num = tokenizador.obtenerTokens(n);
+		Assert.fail();
+	    }catch(ExcepcionExpresionInvalida eei){}
+	}
+	
+	//Prueba aleatoria para números.
+	String[] aleatorios = numerosAleatorios();
+	String aleatoriosValidos = "";
+	String aleatoriosInvalidos = "";
+	for(String n : aleatorios){
+	    aleatoriosValidos += n + " ";
+	    aleatoriosInvalidos += n;
+	}
+	Lista<String> aleatoriosLista = tokenizador.obtenerTokens(aleatoriosValidos);
+	Assert.assertTrue(aleatoriosLista.getLongitud() == aleatorios.length);
+	i = 0;
+	for(String n : aleatoriosLista)
+	    Assert.assertTrue(n.equals(aleatorios[i++]));
+	try{
+	    Lista<String> aleatoriosLista2 = tokenizador.obtenerTokens(aleatoriosInvalidos);
+	    Assert.fail();
+	}catch(ExcepcionExpresionInvalida eei){}
+		
+	//Prueba fija para cadenas.
+	String expr = "";
+	String expr2 = "";
+	int r = random.nextInt(100);
+	for(i = 0; i < r; i++){
+	    int rand = random.nextInt(15);
+	    expr += expresiones[rand];
+	    expr2 += expresiones[rand] + " ";
+	}
+	Lista<String> tokens = tokenizador.obtenerTokens(expr);
+	Lista<String> tokens2 = tokenizador.obtenerTokens(expr2);
+	Assert.assertTrue(tokens.equals(tokens2));
+
+	//Prueba aleatoria para cadenas.
+	for(i = random.nextInt(50); i > 0; i--){
+	    try{
+		Lista<String> tokensAleatorios = tokenizador.obtenerTokens(cadenaAleatoria());
+		Assert.fail();
+	    }catch(ExcepcionExpresionInvalida eei){}
+	}
+    }
+
+    /* Genera un arreglo con números aleatorios válidos. */
+    private String[] numerosAleatorios(){
+	int r = random.nextInt(50);
+	String[] numerosAleatorios = new String[r];
+	for(int i = 0; i < r; i++){
+	    String n = String.valueOf(random.nextDouble()*random.nextInt(100));
+	    numerosAleatorios[i] = n;
+	}
+	return numerosAleatorios;
+    }
+
+    /* Genera una cadena aleatoria. */
+    private String cadenaAleatoria(){
+	int r = random.nextInt(50);
+	byte[] bytes = new byte[r];
+	random.nextBytes(bytes);
+	String cadena = "";
+	for(int i = 0; i < r; i++){
+	    char c = (char)(bytes[i] << (random.nextInt(9)));
+	    cadena += c;
+	}
+	return cadena;
+    }
 }
