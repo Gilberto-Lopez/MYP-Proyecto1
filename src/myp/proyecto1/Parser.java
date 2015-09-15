5edd21ffb6994de981472abb0df5346096150f85
Gilberto-Lopez <gilisaac@ciencias.unam.mx>
2015-09-14 20:04:20 -0500

    Cambios: Cambios de implementación en Parser.java Cambios en pruebas unitarias de Parser.java en TestParser.java

--- src/myp/proyecto1/Parser.java
+++ src/myp/proyecto1/Parser.java
@@ -1,9 +1,8 @@
 package myp.proyecto1;
 
 import mx.unam.ciencias.edd.Lista;
+import mx.unam.ciencias.edd.IteradorLista;
 import mx.unam.ciencias.edd.Pila;
-import mx.unam.ciencias.edd.Cola;
-import java.util.NoSuchElementException;
 
 /**
  * Clase para analizar ecuaciones matemáticas en
@@ -27,70 +26,117 @@ public class Parser{
     /**
      * Analiza (parsing) la ecuación matématica (función) , la cual ya ha sido
      * divida en tokens (fichas) y es recibida como una lista, la convierte a
-     * Notación Polaca Inversa y la devuelve dentro de una cola.
+     * Notación Polaca Inversa y la devuelve dentro de una lista.
      * @param tokens La lista de tokens de la ecuación matemática.
-     * @return Una cola con la ecuación matemática en Notación Polaca Inversa.
+     * @return Una lista con la ecuación matemática en Notación Polaca Inversa.
      * @throws ExcepcionExpresionInvalida en caso de que la ecuación no sea una
      *         expresión válida.
      */
-    public Cola<String> parsea(Lista<String> tokens){
-	Cola<String> cola = new Cola<>();
-	int l = tokens.getLongitud();
-	for(int i = 0; i < l; i++){
-	    String t = tokens.get(i);
-	    if(Utils.esVariable(t))
-		cola.mete(t);
-	    else if(Utils.esNumero(t))
-		cola.mete(t);
-	    else if(Utils.esFuncion(t))
-		pila.mete(t);
-	    else if(Utils.esOperador(t)){
-		while(!pila.esVacia() && Utils.esOperador(pila.mira()) &&
-		      ((t.equals("^") &&
-			Utils.comparaPrecedencia(t, pila.mira()) < 0) ||
-		       (!t.equals("^") &&
-			Utils.comparaPrecedencia(t, pila.mira()) <= 0))){
-		    cola.mete(pila.saca());
+    public Lista<String> parsea(Lista<String> tokens){
+	Lista<String> lista = new Lista<>();
+	IteradorLista<String> iterador = tokens.iteradorLista();
+	while(iterador.hasNext()){
+	    String t = iterador.next();
+	    if(Utils.esVariable(t) || Utils.esNumero(t)){
+		lista.agrega(t);
+		if(iterador.hasNext()){
+		    String sig = iterador.next();
+		    if(!sig.equals(")") && !Utils.esOperador(sig))
+			Utils.excepcion("\"" + t + sig +
+					"\" no es una expresión válida.");
+		    iterador.previous();
 		}
+	    }else if(Utils.esFuncion(t)){
 		pila.mete(t);
-		while(tokens.get(i+1).equals("-")){
-		    i++;
-		    cola.mete("0.0");
-		    pila.mete("-");
-		}
+		if(iterador.hasNext()){
+		    String sig = iterador.next();
+		    if(!sig.equals("("))
+			Utils.excepcion("\"" + t + sig +
+					"\" no es una expresión válida.");
+		    iterador.previous();
+		}else
+		    //Se acabaron los tokens y t no tiene argumentos.
+		    Utils.excepcion("\"" + t + "\" no tiene argumentos.");
+	    }else if(Utils.esOperador(t)){
+		while(!pila.esVacia() && !t.equals("^") &&
+		      Utils.esOperador(pila.mira()) &&
+		      Utils.comparaPrecedencia(t, pila.mira()) <= 0)
+		    lista.agrega(pila.saca());
+		pila.mete(t);
+		if(iterador.hasNext()){
+		    String sig = iterador.next();
+		    if((Utils.esOperador(sig) && !sig.equals("-")) ||
+		       sig.equals(")"))
+			Utils.excepcion("\"" + t + sig +
+					"\" no es una expresión válida.");
+		    iterador.previous();
+		}else
+		    //Se acabaron los tokens y t no tiene operando derecho.
+		    Utils.excepcion("\"" + t + "\" no tiene el número correcto de operandos.");
+		agregaMenos(iterador, lista);
 	    }else if(t.equals("(")){
-		if(tokens.get(i+1).equals(")"))
-		    Utils.excepcion("() no es una expresión válida.");
 		pila.mete(t);
-		while(tokens.get(i+1).equals("-")){
-		    i++;
-		    cola.mete("0.0");
-		    pila.mete("-");
-		}
-	    }else if(t.equals(")")){
-		try{
-		    while(!pila.mira().equals("("))
-			cola.mete(pila.saca());
-		}catch(NoSuchElementException nsee){
+		if(iterador.hasNext()){
+		    String sig = iterador.next();
+		    if((Utils.esOperador(sig) && !sig.equals("-")) ||
+		       sig.equals(")"))
+			Utils.excepcion("\"(" + sig +
+					"\" no es una expresión válida.");
+		    iterador.previous();
+		}else
+		    //Se acabaron los tokens y t no tiene una pareja ")".
 		    Utils.excepcion("Los paréntesis no están bien balanceados.");
+		agregaMenos(iterador, lista);
+	    }else{ //t.equals(")")
+		while(!pila.esVacia() && !pila.mira().equals("("))
+		    lista.agrega(pila.saca());
+		if(!pila.esVacia())
+		    pila.saca();
+		else
+		    //Se acabó la pila sin encontrar un paréntesis "("
+		    Utils.excepcion("Los paréntesis no están bien balanceados.");
+		if(!pila.esVacia() && Utils.esFuncion(pila.mira()))
+		    lista.agrega(pila.saca());
+		if(iterador.hasNext()){
+		    String sig = iterador.next();
+		    if(!Utils.esOperador(sig) && !sig.equals(")"))
+			Utils.excepcion("\"" + t + sig +
+					"\" no es una expresión válida.");
+		    iterador.previous();
 		}
-		pila.saca();
-		try{
-		    if(Utils.esFuncion(pila.mira()))
-			cola.mete(pila.saca());
-		}catch(NoSuchElementException nsee){
-		    continue;
-		}
-	    }else
-		Utils.excepcion("\"" + t + "\" no es una expresión válida.");
+	    }
 	}
 	while(!pila.esVacia()){
 	    String t = pila.saca();
 	    if(t.equals("(") || t.equals(")"))
 		Utils.excepcion("Los paréntesis no están bien balanceados.");
-	    cola.mete(t);
+	    lista.agrega(t);
 	}
-	return cola;
+	return lista;
     }
 
+    /* Verifica si hay una secuencia de menos "-" con un operando.
+     * Ejemplo: ---x. */
+    private void agregaMenos(IteradorLista<String> iterador,
+			     Lista<String> lista){
+	//Sabemos que el i-ésimo más un token existe.
+	int flag = 0;
+	while(iterador.hasNext()){
+	    String t = iterador.next();
+	    if(t.equals("-")){
+		flag++;
+		lista.agrega("0.0");
+		pila.mete("-");
+	    }else if(t.equals(")") && flag != 0){
+		Utils.excepcion("\"-)\" no es una expresión válida.");
+	    }else{
+		iterador.previous();
+		break;
+	    }
+	}
+	if(!iterador.hasNext() && flag != 0)
+	    //Se acabaron los tokens y hay un "-" sin operando derecho.
+	    Utils.excepcion("\"-\" no tiene el número correcto de operandos.");
+    }
+    
 }
--- src/myp/proyecto1/test/TestParser.java
+++ src/myp/proyecto1/test/TestParser.java
@@ -4,7 +4,7 @@ import java.util.Random;
 import myp.proyecto1.Parser;
 import myp.proyecto1.Tokens;
 import myp.proyecto1.ExcepcionExpresionInvalida;
-import mx.unam.ciencias.edd.Cola;
+import mx.unam.ciencias.edd.Lista;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -30,25 +30,36 @@ public class TestParser{
      * Prueba unitaria para {@link Parser#parsea}.
      */
     @Test public void testParsea(){
-	int i = 0;
+	int i;
 	//Casos fijos
 	//f1(x) = 12 + x * 9
-	Cola<String> f1 = parser.parsea(tokenizador.obtenerTokens("12+x*9"));
+	i = 0;
+	Lista<String> f1 = parser.parsea(tokenizador.obtenerTokens("12+x*9"));
 	String[] f1t = {"12","x","9","*","+"};
-	for(String t : f1t)
-	    Assert.assertTrue(t.equals(f1.saca()));
+	for(String t : f1)
+	    Assert.assertTrue(t.equals(f1t[i++]));
 	//f2(x) = x ^ 9 - 10 / 3
-	Cola<String> f2 = parser.parsea(tokenizador.obtenerTokens("x^9-10/3"));
+	i = 0;
+	Lista<String> f2 =
+	    parser.parsea(tokenizador.obtenerTokens("(x^9-(10/3))"));
 	String[] f2t = {"x","9","^","10","3","/","-"};
-	for(String t : f2t)
-	    Assert.assertTrue(t.equals(f2.saca()));
+	for(String t : f2)
+	    Assert.assertTrue(t.equals(f2t[i++]));
 	//f3(x) = cos(-x)
-	Cola<String> f3 = parser.parsea(tokenizador.obtenerTokens("cos(-x)"));
+	i = 0;
+	Lista<String> f3 = parser.parsea(tokenizador.obtenerTokens("cos(-x)"));
 	String[] f3t = {"0.0","x","-","cos"};
-	for(String t : f3t)
-	    Assert.assertTrue(t.equals(f3.saca()));
+	for(String t : f3)
+	    Assert.assertTrue(t.equals(f3t[i++]));
+	Lista<String> f;
+	//Caso especial
+	//----3
+	i = 0;
+	f = parser.parsea(tokenizador.obtenerTokens("----3"));
+	String[] ft = {"0.0","0.0","0.0","3","-","-","-","-"};
+	for(String t : f)
+	    Assert.assertTrue(t.equals(ft[i++]));
 	//Entradas no manejables
-	Cola<String> f;
 	//() dos paréntesis.
 	try{
 	    f = parser.parsea(tokenizador.obtenerTokens("()"));
@@ -56,7 +67,7 @@ public class TestParser{
 	}catch(ExcepcionExpresionInvalida eei){
 	    System.out.println(eei.getMessage());
 	}
-	//+* dos operadores juntos, salvo "-".
+	//*+ dos operadores juntos, salvo "-".
 	try{
 	    f = parser.parsea(tokenizador.obtenerTokens("*+"));
 	    Assert.fail();
@@ -105,7 +116,7 @@ public class TestParser{
 	}catch(ExcepcionExpresionInvalida eei){
 	    System.out.println(eei.getMessage());
 	}
-	//)x paréntesis de ceirre seguido de algo que no es operador.
+	//)x paréntesis de cierre seguido de algo que no es operador o ")".
 	try{
 	    f = parser.parsea(tokenizador.obtenerTokens(")x"));
 	    Assert.fail();
@@ -119,6 +130,8 @@ public class TestParser{
 	}catch(ExcepcionExpresionInvalida eei){
 	    System.out.println(eei.getMessage());
 	}
+	f = parser.parsea(tokenizador.obtenerTokens("(x^4+(16*x+--3)^3-74*x+16)/2^3"));
+	f = parser.parsea(tokenizador.obtenerTokens("x^2+-3*x+(15^2-2)"));
     }
 
 }
